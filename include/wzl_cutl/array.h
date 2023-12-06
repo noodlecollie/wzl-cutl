6a17b0cd15dda27cb32a228c0785f889b998d4b9
Jonathan Poncelet <j.poncelet@efg.gg>
2023-12-06 17:03:13 +0000

    Added array header

---
+++ include/wzl_cutl/array.h
@@ -0,0 +1,9 @@
+// File: array.h
+// Defines helper functions and macros for use with arrays.
+
+#ifndef WZL_CUTL_ARRAY_H
+#define WZL_CUTL_ARRAY_H
+
+#define WZL_ARRAY_SIZE(array) (sizeof((array)) / sizeof((array)[0]))
+
+#endif  // WZL_CUTL_ARRAY_H
